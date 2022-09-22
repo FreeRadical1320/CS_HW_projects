de0ba72a6f535ca2a95a6799dfd2ae37ef3faa6c
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-22 12:25:52 +0300

    Task 13

---
+++ CS_HW_02/Task_13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Please, input x = ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input which right digit do you want to find: ");
+
+int nRight = Convert.ToInt32(Console.ReadLine());
+
+
+int rightDigit = -1;
+
+for(int i = 0; i < nRight; i++)
+{
+    if(x != 0)
+    {
+        rightDigit = x % 10;
+        x /= 10;
+    }
+    else
+    {
+        Console.WriteLine($"{nRight} digit is not exist!");
+        break;
+    }
+}
+
+Console.WriteLine($"{nRight} digit is {rightDigit}");
