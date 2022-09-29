ab0076cf5ed1d194adb5c4ac90f7b697c5d930c2
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-29 12:54:45 +0300

    Task_29

---
+++ CS_HW_04/Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] arr = new int[8];
+
+for(int i = 0; i < 8; i++)
+{
+    Console.Write($"Please, input  number {i + 1}: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("[");
+for(int i = 0; i < 8; i++)
+{
+    Console.Write($"{arr[i]}, ");
+}
+Console.WriteLine("]");
