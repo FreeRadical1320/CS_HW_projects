b2655d219c035f07e91193ca43fc5927777060ed
FreeRadical1320 <vlad.pronnikov@gmail.com>
2022-09-29 12:34:14 +0300

    Task_25

---
+++ CS_HW_04/Task_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Please, input A = ");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please, input B = ");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+int prod = 1;
+
+for(int i = 1; i <= B; i++)
+{
+   prod *= A ;
+}
+Console.WriteLine($"A raised to the power of B is equal: {prod}."); 
